12fa6ea94063e6585ac8702ba291b621d36176a4
Lucas Baamonde <lucas.baamonde@travelgenio.com>
2019-11-11 20:05:00 -0300

    change detail target

--- src/actions/cardList/cardList.action.ts
+++ src/actions/cardList/cardList.action.ts
@@ -2,7 +2,6 @@ import { ThunkAction } from 'redux-thunk';
 import { RootState } from '../../store';
 import { RootAction } from '../action';
 import { SEARCH_ACCOMMODATION_SELECT, ResultActionTypes } from './cardList.actionTypes';
-import { push } from 'connected-react-router';
 
 export type ThunkResult<R> = ThunkAction<R, RootState, undefined, RootAction>;
 
@@ -16,5 +15,5 @@ export function accommodationSelect(id: string) : ResultActionTypes {
 export const thunkAccommodationSelect = (accommodationId: string) => async (
   dispatch
 ) => {
-  window.open("http://d1m9g3t59jyyir.cloudfront.net/Hotel%20Hilton-"+ accommodationId + "/2019-11-15/2019-11-24/2");
+  window.open("http://d1m9g3t59jyyir.cloudfront.net/Hotel%20Hilton-"+ accommodationId + "/2019-11-15/2019-11-24/2/es");
 };
