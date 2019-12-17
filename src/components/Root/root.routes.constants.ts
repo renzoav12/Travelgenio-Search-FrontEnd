0fc113b01989d6d0620cabb5e6a947bef44cef0d
Lucas Baamonde <lucas.baamonde@travelgenio.com>
2019-12-17 17:55:32 -0300

    change router

--- src/components/Root/root.routes.constants.ts
+++ src/components/Root/root.routes.constants.ts
@@ -1,4 +1,4 @@
 // tslint:disable:max-line-length
-export const ROUTE_NOT_FOUND = '/404/'
-export const ROUTE_SEARCH ='/'
-export const ROUTE_SEARCH_RESULTS ='/:locationType/:locationCode/:stayFrom/:stayTo/:occupancy/:language'
+export const ROUTE_NOT_FOUND = '/hotels/search/404'
+export const ROUTE_SEARCH ='/hotels/search'
+export const ROUTE_SEARCH_RESULTS ='/hotels/search/:locationType/:locationCode/:stayFrom/:stayTo/:occupancy/:language'
