11f85f81693b0af9f078c7eb0e97cd8703c2ee5d
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-06-30 16:03:14 -0300

    Redirect to home if the route is invalid.

--- src/components/Root/root.routes.tsx
+++ src/components/Root/root.routes.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Route, Switch } from "react-router";
+import { Route, Switch, Redirect } from "react-router";
 
 import { ROUTE_SEARCH, ROUTE_SEARCH_RESULTS, ROUTE_OLD_SEARCH_RESULTS } from "./root.routes.constants";
 import SearchContainer from "../../containers/Search/SearchContainer";
 
+const homeUrl = window.location.protocol+"//"+window.location.host + "/hotels/home/";
+
 export const getRoutes = () => (
   <div>
     <Switch>
-      <Route exact path={ROUTE_SEARCH} component={SearchContainer} />
       <Route exact path={ROUTE_SEARCH_RESULTS} component={SearchContainer} />
       <Route exact path={ROUTE_OLD_SEARCH_RESULTS} component={SearchContainer} />
+      <Route path={ROUTE_SEARCH} render={() => {window.location.href = homeUrl; return null;}}/>
     </Switch>
   </div>
 );
