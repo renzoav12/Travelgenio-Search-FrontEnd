650f0ab81dfbcdb10888f08195b0c406752ae96f
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-09-04 19:06:10 -0300

    Distance Km to Meters

--- src/components/Card/Content/Distance/Distance.tsx
+++ src/components/Card/Content/Distance/Distance.tsx
@@ -3,6 +3,7 @@ import { Box } from "@material-ui/core";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Keys from "@hotels/translation-keys";
 import Translate from "@hotels/translation";
+import { cloneDeep } from "lodash";
 
 export interface Props {
   distance: DistanceProps;
@@ -20,9 +21,15 @@ export interface RegionProps {
   type: string;
 }
 
-const specificPlaceTypes = ["neighborhood", "airport", "point_of_interest", "train_station", "metro_station"];
+const specificPlaceTypes = [
+  "airport",
+  "point_of_interest",
+  "train_station",
+  "metro_station",
+];
 
 const meters = "m";
+const kilometers = "km";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -30,44 +37,66 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "flex",
       height: "100%",
       color: "#777777",
-      fontSize: "9pt"
+      fontSize: "9pt",
     },
   })
 );
 
+const adjustUnit = (distance: DistanceProps): DistanceProps => {
+  const adjustedDistance = cloneDeep(distance);
+
+  if (adjustedDistance.unit === kilometers && adjustedDistance.value < 1) {
+    adjustedDistance.value = adjustedDistance.value * 1000;
+    adjustedDistance.unit = meters;
+  }
+
+  return adjustedDistance;
+};
+
+const selectKey = (distance: DistanceProps) => {
+  if (specificPlaceTypes.includes(distance.region.type.toLowerCase())) {
+    if (distance.unit === meters) {
+      return Keys.search.distance_meters_from;
+    } else {
+      return Keys.search.distance_kilometers_from;
+    }
+  } else {
+    if (distance.unit === meters) {
+      return Keys.search.distance_meters_from_center;
+    } else {
+      return Keys.search.distance_kilometers_from_center;
+    }
+  }
+};
+
+const createValues = (distance: DistanceProps) => {
+  const value =
+    distance.unit === meters ? distance.value : distance.value.toFixed(1);
+  return { n: value, m: distance.region.name };
+};
+
+
+const formatDistance = (distance: DistanceProps) => {
+  if (distance) {
+    const adjustedDistance = adjustUnit(distance);
+
+    return (
+      <Translate
+        tkey={selectKey(adjustedDistance)}
+        values={createValues(adjustedDistance)}
+      />
+    );
+  } else {
+    return "";
+  }
+};
+
 const Distance: FunctionComponent<Props> = (props) => {
   const classes = useStyles();
 
-  const formatDistance = (distance: DistanceProps) => {
-    if(distance) {
-      const distanceValue = (props.distance.unit === meters ? props.distance.value : props.distance.value.toFixed(1));
-      const values = {n: distanceValue, m: distance.region.name};
-      let key;
-
-      if(specificPlaceTypes.includes(props.distance.region.type.toLowerCase())) {
-        if(props.distance.unit === meters) {
-          key = Keys.search.distance_meters_from;
-        } else {
-          key = Keys.search.distance_kilometers_from;
-        }
-      } else {
-        if(props.distance.unit === meters) {
-          key = Keys.search.distance_meters_from_center;
-        } else {
-          key = Keys.search.distance_kilometers_from_center;
-        }
-      }
-
-      return <Translate tkey={key} values={values} />
-    }else {
-      return "";
-    }
-  }
-
-
-  return <Box className={classes.distance}>
-    {formatDistance(props.distance)}
-  </Box>;
+  return (
+    <Box className={classes.distance}>{formatDistance(props.distance)}</Box>
+  );
 };
 
 export default Distance;
